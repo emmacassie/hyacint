bdd4b6b5775fccb885e7b740e8100a2f86cc53d5
emmacassie <96371921+emmacassie@users.noreply.github.com>
2021-12-19 12:17:52 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,16 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const greetings = {morning: ["goodmorning", "hey early bird", "its good to see you", "rise and shine!"], afternoon: ["good afternoon", "good day"], evening: ["good evening", "sleep tight"], night: ["hi u tiny bat", ]}
+  let currentGreeting = "Hello" 
+  const currentHour = new Date().getHours()
+  const currentMinute = new Date().getMinutes()
+  if ((currentHour >= 5 && currentHour < 12) || (currentHour === 5 && currentMinute > 30)) {currentGreeting = greetings.morning[0]}
+  if ((currentHour >= 12 ) && (currentHour < 17))  {currentGreeting = greetings.afternoon[1]}
+  if ((currentHour >= 17 && currentHour < 23) || (currentHour === 23 && currentMinute < 30))  {currentGreeting = greetings.evening[0]}
+  if (currentHour >= 0 && currentHour < 5) || (currentHour === 23 && currentMinute > 30))  {currentGreeting = greetings.night[1]}
+
+  
   return (
     <div className={styles.container}>
       <Head>
@@ -13,7 +23,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Good morning &lt;3
+          {currentGreeting} &lt;3
         </h1>
 
       </main>
