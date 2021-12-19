ac13718c14b1119725fe19b6e81eeb860c6ec636
emmacassie <96371921+emmacassie@users.noreply.github.com>
2021-12-19 12:18:49 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
   if ((currentHour >= 5 && currentHour < 12) || (currentHour === 5 && currentMinute > 30)) {currentGreeting = greetings.morning[0]}
   if ((currentHour >= 12 ) && (currentHour < 17))  {currentGreeting = greetings.afternoon[1]}
   if ((currentHour >= 17 && currentHour < 23) || (currentHour === 23 && currentMinute < 30))  {currentGreeting = greetings.evening[0]}
-  if (currentHour >= 0 && currentHour < 5) || (currentHour === 23 && currentMinute > 30))  {currentGreeting = greetings.night[1]}
+  if (currentHour >= 0 && currentHour < 5) || (currentHour === 23 && currentMinute > 30))  {currentGreeting = greetings.night[0]}
 
   
   return (
