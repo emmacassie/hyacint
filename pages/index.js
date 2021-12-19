15535bf8162fadba954f0010baaf9262aadb96db
emmacassie <96371921+emmacassie@users.noreply.github.com>
2021-12-19 11:11:45 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Good morning &lt; 3
+          Good morning &lt;3
         </h1>
 
         <p className={styles.description}>
