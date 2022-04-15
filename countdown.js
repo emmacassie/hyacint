4b01a6cd39d4a6209c931a473dac592833e5aca1
Solomon Omojola <omojolasolomon@gmail.com>
2022-04-15 22:29:47 +0200

    clear

--- countdown.js
+++ countdown.js
@@ -4,37 +4,46 @@
 
 // current time
 
-const TARGET = 1647788400000;
-function getCurrentTime() {
-  return Date.now();
-}
+fetch("https://ergast.com/api/f1/current/next.json")
+  .then((response) => response.json())
+  .then((data) => {
+    const nextStart = new Date(
+      data.MRData.RaceTable.Races[0].date +
+        "T" +
+        data.MRData.RaceTable.Races[0].time
+    )
+    const TARGET = nextStart.getTime()
+    function getCurrentTime() {
+      return Date.now()
+    }
 
-function doThisEverySecond(fn) {
-  setInterval(fn, 1000);
-}
+    function doThisEverySecond(fn) {
+      setInterval(fn, 1000)
+    }
 
-function calculateTimeLeft(current) {
-  let timeDiff = TARGET - current;
-  const secondsFactor = 1000;
-  const minutesFactor = secondsFactor * 60;
-  const hoursFactor = minutesFactor * 60;
-  const daysFactor = hoursFactor * 24;
-  const daysRemaining = Math.floor(timeDiff / daysFactor);
-  timeDiff = timeDiff - (daysFactor * daysRemaining);
-  const hoursRemaining = Math.floor(timeDiff  / hoursFactor);
-  timeDiff = timeDiff - (hoursRemaining * hoursFactor);
-  const minutesRemaining = Math.floor(timeDiff / minutesFactor );
-  timeDiff = timeDiff - (minutesRemaining * minutesFactor);
-  const secondsRemaining = Math.floor(timeDiff  / secondsFactor );
+    function calculateTimeLeft(current) {
+      let timeDiff = TARGET - current
+      const secondsFactor = 1000
+      const minutesFactor = secondsFactor * 60
+      const hoursFactor = minutesFactor * 60
+      const daysFactor = hoursFactor * 24
+      const daysRemaining = Math.floor(timeDiff / daysFactor)
+      timeDiff = timeDiff - daysFactor * daysRemaining
+      const hoursRemaining = Math.floor(timeDiff / hoursFactor)
+      timeDiff = timeDiff - hoursRemaining * hoursFactor
+      const minutesRemaining = Math.floor(timeDiff / minutesFactor)
+      timeDiff = timeDiff - minutesRemaining * minutesFactor
+      const secondsRemaining = Math.floor(timeDiff / secondsFactor)
 
-  return `${daysRemaining} days : ${hoursRemaining} hours : ${minutesRemaining} minutes :  ${secondsRemaining} seconds`;
-}
+      return `${daysRemaining} days : ${hoursRemaining} hours : ${minutesRemaining} minutes :  ${secondsRemaining} seconds`
+    }
 
-function displayTimeLeft() {
-  const currentTime = getCurrentTime();
-  const timeLeft = calculateTimeLeft(currentTime);
-  const element = window.document.querySelector("#time-goes-here");
-  element.textContent = "Countdown: " + timeLeft;
-}
+    function displayTimeLeft() {
+      const currentTime = getCurrentTime()
+      const timeLeft = calculateTimeLeft(currentTime)
+      const element = window.document.querySelector("#time-goes-here")
+      element.textContent = "Countdown: " + timeLeft
+    }
 
-doThisEverySecond(displayTimeLeft);
+    doThisEverySecond(displayTimeLeft)
+  })
